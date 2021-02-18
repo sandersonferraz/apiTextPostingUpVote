e4d8ebd61c87d4245ba63561e1509241c44cc88c
Sanderson Ferraz <sandersonferraz@gmail.com>
2021-02-18 16:29:21 -0300

    Created TextPostServiceTest and TextPostService

--- src/main/java/com/segware/textPostUpvote/service/TextPostService.java
+++ src/main/java/com/segware/textPostUpvote/service/TextPostService.java
@@ -5,6 +5,7 @@ import com.segware.textPostUpvote.model.TextPost;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 
 import java.util.List;
 
@@ -15,10 +16,42 @@ public class TextPostService {
     TextPostRepository textPostRepository;
 
     public TextPost create(@PathVariable TextPost textPost){
+        textPost.setVote(0);
         return textPostRepository.save(textPost);
     }
 
     public List<TextPost> findAll(){
         return textPostRepository.findAll();
+    }
+
+    public TextPost findById(@PathVariable String id) {
+        return textPostRepository.findById(id).orElse(null);
+    }
+
+    public TextPost update(@RequestBody TextPost updateText) {
+        TextPost text = textPostRepository.findById(updateText.getId()).orElse(null);
+        assert text != null;
+        text.setText(updateText.getText());
+        textPostRepository.save(text);
+        return text;
+    }
+
+
+    public String delete(@PathVariable String id) {
+        textPostRepository.deleteById(id);
+        return id;
+    }
+
+    public String upvoteText(@PathVariable String id) {
+        TextPost textPost = textPostRepository.findById(id).orElse(null);
+        textPost.setVote(textPost.getVote() + 1);
+        return id;
+
+    }
+    public String downVoteText(@PathVariable String id) {
+        TextPost textPost = textPostRepository.findById(id).orElse(null);
+        textPost.setVote(textPost.getVote() - 1);
+        return id;
+
     }
 }
--- src/test/java/com/segware/textPostUpvote/TextPostServiceTest.java
+++ src/test/java/com/segware/textPostUpvote/TextPostServiceTest.java
@@ -3,34 +3,130 @@ package com.segware.textPostUpvote;
 
 import com.segware.textPostUpvote.model.TextPost;
 import com.segware.textPostUpvote.service.TextPostService;
-import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
 
-@SpringBootTest
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.*;
+
+
+@RunWith(MockitoJUnitRunner.class)
 public class TextPostServiceTest {
 
-    @Autowired
+    @InjectMocks
     private TextPostService service;
 
-    @Test
-    public void testServiceCreate() {
-
-        TextPost newText = new TextPost();
-        newText.setText("Lorem Ipsum is simply dummy text of the printing and typesetting " +
-                "industry. Lorem Ipsum has been the industry's standard dummy text ever " +
-                        "since the 1500s, when an unknown printer took a galley of type and scrambled " +
-                        "it to make a type specimen book.");
-
-        service.create(newText);
-    }
+    @Mock
+    TextPostRepository repository;
 
 
     @Test
     public void testServiceFindAll() {
-       service.findAll();
+        List<TextPost> listAll = new ArrayList<>();
+        TextPost textOne = new TextPost();
+        textOne.setText("Lorem Ipsum is simply dummy text of the printing and typesetting " +
+                "industry. Lorem Ipsum has been the industry's standard dummy text ever " +
+                "since the 1500s, when an unknown printer took a galley of type and scrambled " +
+                "it to make a type specimen book.");
+
+        TextPost textTwo = new TextPost();
+        textTwo.setText("It is a long established fact that a reader will be distracted by the readable " +
+                "content of a page when looking at its layout. The point of using Lorem Ipsum is that it " +
+                "has a more-or-less normal distribution of letters, as opposed to using 'Content here, " +
+                "content here', making it look like readable English. ");
+
+        listAll.add(textOne);
+        listAll.add(textTwo);
+
+        when(repository.findAll()).thenReturn(listAll);
+
+        List<TextPost> textPostListAll = service.findAll();
+
+        assertEquals(2, textPostListAll.size());
+        verify(repository, times(1)).findAll();
+
     }
 
+
+    @Test
+    public void testServiceFindById() {
+        when(repository.findById("q1w2e3r4t5")).thenReturn(java.util.Optional.of(new TextPost(
+                "q1w2e3r4t5",
+                "It is a long established fact that a reader will be distracted by ",
+                0)));
+        TextPost text = service.findById("q1w2e3r4t5");
+
+        assertEquals("q1w2e3r4t5", text.getId());
+        assertEquals("It is a long established fact that a reader will be distracted by ", text.getText());
+        assertEquals(0, text.getVote());
+
+    }
+
+    @Test
+    public void testServiceCreate() {
+
+        TextPost textPost = new TextPost(
+                "q1w2e3r4t5",
+                "It is a long established fact that a reader will be distracted by ",
+                0);
+        service.create(textPost);
+        verify(repository, times(1)).save(textPost);
+
+    }
+
+
+    @Test
+    public void testServiceUpdate() {
+
+        when(repository.findById("q1w2e3r4t5")).thenReturn(java.util.Optional.of(new TextPost(
+                "q1w2e3r4t5",
+                "It is a long established fact that a reader will be distracted by ",
+                0)));
+
+        TextPost text = service.findById("q1w2e3r4t5");
+        text.setText("Outro text!!!");
+
+        service.update(text);
+        assertEquals("Outro text!!!", service.findById("q1w2e3r4t5").getText());
+
+    }
+
+
+    @Test
+    public void testServiceUpVote() {
+
+        when(repository.findById("q1w2e3r4t5")).thenReturn(java.util.Optional.of(new TextPost(
+                "q1w2e3r4t5",
+                "It is a long established fact that a reader will be distracted by ",
+                0)));
+
+        TextPost text = service.findById("q1w2e3r4t5");
+        service.upvoteText(text.getId());
+
+        assertEquals(1, service.findById("q1w2e3r4t5").getVote());
+
+    }
+
+
+    @Test
+    public void testServiceDownVote() {
+        when(repository.findById("q1w2e3r4t5")).thenReturn(java.util.Optional.of(new TextPost(
+                "q1w2e3r4t5",
+                "It is a long established fact that a reader will be distracted by ",
+                1)));
+
+        TextPost text = service.findById("q1w2e3r4t5");
+        service.downVoteText(text.getId());
+
+        assertEquals(0, service.findById("q1w2e3r4t5").getVote());
+
+    }
 
 
 }
