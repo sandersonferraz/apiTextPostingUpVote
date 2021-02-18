a879ed97714522a8e0c12abcb127b7f3f5232bce
Sanderson Ferraz <sandersonferraz@gmail.com>
2021-02-18 17:03:28 -0300

    Created TextPostController

---
+++ src/main/java/com/segware/textPostUpvote/controller/TextPostController.java
@@ -0,0 +1,67 @@
+package com.segware.textPostUpvote.controller;
+
+import com.segware.textPostUpvote.model.TextPost;
+import com.segware.textPostUpvote.service.TextPostService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+public class TextPostController {
+
+    @Autowired
+    TextPostService textPostService;
+
+    @GetMapping
+    @RequestMapping("/")
+    @ResponseStatus(HttpStatus.OK)
+    public List<TextPost> findAll() {
+        return textPostService.findAll();
+    }
+
+    @GetMapping("/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public TextPost findById(@PathVariable String id) {
+        return textPostService.findById(id);
+    }
+
+    @PostMapping("/")
+    @ResponseStatus(HttpStatus.CREATED)
+    public TextPost create(@RequestBody TextPost textPost) {
+        return textPostService.create(textPost);
+    }
+
+    @PutMapping("/")
+    @ResponseStatus(HttpStatus.OK)
+    public TextPost update(@RequestBody TextPost newTextPost) {
+        TextPost textPost = textPostService.findById(newTextPost.getId());
+        textPost.setText(newTextPost.getText());
+        textPostService.update(textPost);
+        return textPost;
+    }
+
+    @DeleteMapping("/")
+    @ResponseStatus(HttpStatus.OK)
+    public String delete(@RequestBody String id) {
+        textPostService.delete(id);
+        return id;
+    }
+
+    @GetMapping("/upvote/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public String upvote(@PathVariable String id){
+        textPostService.upvoteText(id);
+        return id;
+    }
+
+    @GetMapping("/downVote/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public String downVote(@PathVariable String id){
+        textPostService.downVoteText(id);
+        return id;
+    }
+
+
+}
